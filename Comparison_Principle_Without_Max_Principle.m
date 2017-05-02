f2ef0163350d148a7b2cc6ff866e7b5299cc2b2e
James Rekow <jamesrekow@gmail.com>
2017-05-02 10:00:53 -0700

    Create Comparison_Principle_Without_Max_Principle.m
    Example illustrating a case where comparison principle holds, but not max principle.

---
+++ Comparison_Principle_Without_Max_Principle.m
@@ -0,0 +1,31 @@
+%James Rekow
+
+%Pure diffusion equation (in u) on (0,1) with Dirichlet BC and an interface
+%at 0.5. The initial condition is u(x,0) = 1, if x < .5, u(x,0) = 0, x > 0.5.
+%The interface conditions are (u_{-})_{x}(0.5-) = 0.25(u_{+})_{x}(0.5+)
+%and u_{-}(0.5-) = 0.25u_{+}(0.5+).
+
+%Consider v = u if x < .5, v = 0.25u if x >0.5.
+%Then equivalent problem in v is a pure diffusion with
+%Dirichlet BC, no interface, and the same initial data.
+
+%In our notation this pair of problems corresponds to 
+%eta_{-} = kappa_{-} = 1, eta_{+} = kappa_{+} = 0.25.
+
+
+%Note that the interface problem in u must obey a comparison principle,
+%since the problem in v does, and multiplication by piecewise constants
+%does not change the ordering of solutions. However, the numerical example
+%clearly shows that u does not obey a maximum principle. The yellow star in the
+%time evolving solution to the u problem represents the maximum value of
+%the solution. It does not attain this maximum on the parabolic boundary.
+%This is also evidenced by the surface in figure2. The ridge is the value
+%of u(0.5+,t).
+
+%Function returns the point (x,t) at which the solution u to the interface
+%problem attains its maximum. The red square on the surface marks the
+%local maximum of u.
+
+[tmax,xmax] = ImplicitHE_Interface_Test(0.02,0.00201)
+
+
